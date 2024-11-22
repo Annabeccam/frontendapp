1c052c7bed6699f59111c91e0b0f70fb5b55b79c
Sheetal <sheetal@Sheetals-MacBook-Pro.local>
2024-11-22 19:20:36 +0200

    deploy angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, UserComponent],
+  //imports: [UserComponent],
+  //templateUrl: './app.component.html',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-user></app-user>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontendapp';
+  title = 'app-frontend';
 }
